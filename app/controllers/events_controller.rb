b82998d7f1491048ac50e204f420f1c42bab69fb
Alexander <alex.khlipun@icloud.com>
2018-02-09 21:09:14 +0200

    Create an event page

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,10 @@
+class EventsController < ApplicationController
+  
+  def index
+    @events = Event.all
+  end
+  
+  def show
+    @event = Event.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "visitors#index"
   devise_for :users
+  resources :events
+  resource :shopping_cart
 end
