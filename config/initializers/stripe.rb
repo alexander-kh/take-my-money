8b776f0af4d3e37e50efef6601f05aacfafb6bda
Alexander <alex.khlipun@icloud.com>
2018-02-21 20:33:54 +0200

    Add Stripe.js library

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,4 @@
 Stripe.api_key = Rails.application.secrets.stripe_secret_key
 raise "Missing Stripe API Key" unless Stripe.api_key
+STRIPE_JS_HOST = "https://js.stripe.com".freeze unless defined? STRIPE_JS_HOST
+STRIPE_JS_FILE = Rails.env.development? ? "stripe-debug.js" : ""
